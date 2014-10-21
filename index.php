69aea70e84eb7b4b837fd6625b7eee8128e77d33
daan <darend@computertraining.nl>
2014-10-21 11:30:57 +0200

    home, footer en newgame aangemaakt

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE>
-<html>
-  	<head>
-    	<title>Hearts 2 GO!</title>
-  	</head>
-	<body>
-		<?php
-		$pageName = 'splash';
-		include_once "_inc/header.php";
-		?>
-		<p>Dit zit in een p element, en heeft woord '<a style="color:red;">rood</a>' in de kleur rood gezet!</p>
-	</body>
-</html>
+<?php
+$pageName = 'splash';
+include_once "_inc/header.php";
+?>
+<a href="home.php">Klik hier om te beginnen</a>

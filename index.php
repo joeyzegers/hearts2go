dfd238e60573f4c95155c8148d79d8c039097041
daan <darend@computertraining.nl>
2014-10-21 09:20:38 +0200

    header correctie

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE>
 <html>
-  <head>
-    <title>Hearts 2 GO!</title>
-  </head>
-<body>
-
-<?php
-$pageName = 'splash';
-include_once "_inc/header.php";
-?>
-    <p>Dit zit in een p element, en heeft woord '<a style="color:red;">rood</a>' in de kleur rood gezet!</p>
-  </body>
+  	<head>
+    	<title>Hearts 2 GO!</title>
+  	</head>
+	<body>
+		<?php
+		$pageName = 'splash';
+		include_once "_inc/header.php";
+		?>
+		<p>Dit zit in een p element, en heeft woord '<a style="color:red;">rood</a>' in de kleur rood gezet!</p>
+	</body>
 </html>

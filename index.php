034babb186c433d064cbbba88c951c336692a434
daan <darend@computertraining.nl>
2014-10-21 08:45:19 +0200

    Header aangemaakt

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
   <head>
     <title>Hearts 2 GO!</title>
   </head>
-  <body>
+<body>
+
+<?php include_once "_inc/header.php";
+?>
     <p>Dit zit in een p element, en heeft woord '<a style="color:red;">rood</a>' in de kleur rood gezet!</p>
   </body>
 </html>
+
